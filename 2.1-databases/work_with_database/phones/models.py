1c425e93559095fc512ae1cee6454b0d5a1e68ff
Ruslan Grigorev <grigor1991@inbox.ru>
2024-11-07 17:18:07 +0300

    Update settings.py, import_phones.py, models.py, views.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True) 
+    name = models.CharField(max_length=200, null=False) 
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    image = models.URLField(max_length=200) 
+    release_date = models.DateField() 
+    lte_exists = models.BooleanField() 
+    slug = models.SlugField(unique=True, blank=True) 
+ 
+    def __str__(self): 
+        return self.name
